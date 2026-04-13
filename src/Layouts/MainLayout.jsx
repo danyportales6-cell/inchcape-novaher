7894479e3a6200283d7c3cdf8a85361192d8dbfa
trivalu23 <niccondooriasto@crackthecode.la>
2026-04-12 20:28:14 -0500

    feat: configurar estructura inicial del proyecto con layout, navbar, footer y rutas

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
+function MainLayout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
---
+++ src/pages/ReportCases.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ReportCases() {
+  return <div>ReportCases</div>;
+}
+
+export default ReportCases;
