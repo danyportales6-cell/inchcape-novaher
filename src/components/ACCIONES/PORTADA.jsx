c9d146ce3bec08948e21a14ff3a032c13aedc468
sharonmartinez7201-cyber <sharonmartinez7201@gmail.com>
2026-04-18 19:57:14 -0500

    POSICION

---
+++ src/components/ACCIONES/PORTADA.jsx
@@ -0,0 +1,15 @@
+import "./Styles/PORTADA01.css";
+
+export default function PORTADA () {
+  return (
+    <section className="hero-peru">
+
+      <div className="top-line"></div>
+
+      <div className="hero-content">
+        <h1>COMPROMETIDOS CON EL CAMBIO</h1>
+      </div>
+
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
 import SECCION1 from "../components/INICIO/SECCION1";
-import SECCION2 from "../components/INICIO/SECCION2";
-import SECCION3 from "../components/INICIO/SECCION3";
-import SECCION4 from "../components/INICIO/SECCION4";
-import SECCION5 from "../components/INICIO/SECCION5";
+import PARTE1 from "../components/ACCIONES/PARTE1";
+import PARTE2 from "../components/ACCIONES/PARTE2";
+import PARTE3 from "../components/ACCIONES/PARTE3";
+import PARTE4 from "../components/ACCIONES/PARTE4";
+import PARTE5 from "../components/ACCIONES/PARTE5";
+import PARTE6 from "../components/ACCIONES/PARTE6";
+import PARTE7 from "../components/ACCIONES/PARTE7";
+import PARTE8 from "../components/ACCIONES/PARTE8";
 
-function Home() {
-
+function Home () {
   return (
-    <div  style={{ 
+    /* Quitamos el height fijo y el hidden para permitir scroll */
+    <div style={{ 
       width: '100%', 
       minHeight: '100vh',    /* Usamos minHeight en lugar de height */
       display: 'flex', 
@@ -16,12 +20,17 @@ function Home() {
       backgroundColor: '#f0f8ff' 
     }}>
       <SECCION1 />
-      <SECCION2/>
-      <SECCION3/>
-      <SECCION4/>
-      <SECCION5/>
+      <PARTE1/>
+      <PARTE2 />
+      <PARTE3/>
+      <PARTE4/>
+      <PARTE5/>
+      <PARTE6/>
+      <PARTE7/>
+      <PARTE8/>
     </div>
-  )
+  );
 }
+
 
 export default Home;
